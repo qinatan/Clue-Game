7ca217fd677878d4e81d3e656f4bd55710f50144
meack2 <34797145+meack2@users.noreply.github.com>
2023-02-25 11:34:25 -0700

    Added mock methods

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,6 +1,24 @@
 package experiment;
 
-public class TestBoard {
+import java.util.Set;
 
-	//test here
+public class TestBoard {
+	
+	/**
+	 * Calculates legal targets for a move from startCell of length pathLength
+	 * @param startCell
+	 * @param pathlength
+	 */
+	void calcTargets (TestBoardCell startCell, int pathlength) {
+		System.out.println("In calcTargets");
+	}
+	
+	TestBoardCell getCell(int row, int col) {
+		System.out.println("In getCell");
+	}
+	
+	Set <TestBoardCell> getTargets() {
+		System.out.println("In getTargets");
+	}
+
 }
